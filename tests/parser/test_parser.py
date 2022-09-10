429736463b9424534d853797079ffb11459f9808
Lucas oliveira <lucasoliveira783@gmail.com>
2022-09-10 09:40:57 -0300

    feat: refatoring file organization

--- tests/parser/test_parser.py
+++ tests/parser/test_parser.py
@@ -1,5 +1,5 @@
 import pytest
-from parser.parser import Parcer
+from src.parser.parser import Parcer
 from .fixtures import mocker_ip_raw_file, EXPECTED_DATA
 
 def test_parcer_success(mocker_ip_raw_file):
@@ -9,4 +9,4 @@ def test_parcer_success(mocker_ip_raw_file):
 def test_parcer_file_not_found():
     parcer = Parcer()
     with pytest.raises(FileNotFoundError):
-        parcer.extract_ip_from_file("fakefile.txt")
+        parcer.extract_ips_from_file("fakefile.txt")
